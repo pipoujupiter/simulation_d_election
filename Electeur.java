934adb4c9e0b6083aa225fbf398ee5a03e52771d
Jupiter <alice.morin007@gmail.com>
2022-01-16 15:16:29 +0100

    Ajout de l'axe dans la classe Personne Passage du type tableau à double + création d'un super constructeur dans la classe Electeur

--- Electeur.java
+++ Electeur.java
@@ -19,10 +19,12 @@ public class Electeur extends Personne {
      * 
      * @param nom    de la personne
      * @param prenom de la personne
-     * @param axe    sociétal de la personne
+     * @param d      paramètre 1 de l'axe sociétal de la personne
+     * @param e      paramètre 2 de l'axe sociétal de la personne
      */
-    public Electeur(String nom, String prenom) {
-        super(nom, prenom);
+
+    public Electeur(String nom, String prenom, double d, double e) {
+        super(nom, prenom, d, e);
 
         this.numeroelecteur = Electeur.NUMERO;
         Electeur.NUMERO++;
--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        Electeur p = new Electeur("Morin", "Alice");
-        Electeur p1 = new Electeur("Valembois", "Auxane");
+        Electeur p = new Electeur("Morin", "Alice", 0.1, 0.9);
+        Electeur p1 = new Electeur("Valembois", "Auxane", 0.2, 0.3);
         System.out.println(p1);
+        System.out.print(p);
     }
 
 }
