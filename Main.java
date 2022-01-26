0f857c657574aed66740887fcb8382597c2ae69d
Jupiter <alice.morin007@gmail.com>
2022-01-26 11:41:39 +0100

    Création d'une interface

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        Candidat c = new Candidat("Monsieur", "Président");
-        c.affCandidat();
-        Scrutin s = new Scrutin();
-        for (int i = 0; i < 100; i++) {
-            Electeur e = new Electeur();
-            s.affPreference(s.preference(e, c));
-        }
-
+        Interface i = new Interface();
+        i.IMH();
     }
 
 }
